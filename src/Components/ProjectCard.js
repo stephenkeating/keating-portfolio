6c936118964d8b5e46f9c18cb3d8334dea338f61
Stephen Keating <stephen.keating@gmail.com>
2020-07-20 12:48:49 -0400

    starting to style project cards

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ProjectCard = ({project: {title, url, description, tech, bullets}}) => {
 
-  return <>
+  return <div className='project-card'>
           { title }
-        </>
+        </div>
 };
 
 export default ProjectCard;
