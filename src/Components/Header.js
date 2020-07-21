b4a314ce3623782e28692cfc32e6a93bccd9635a
Stephen Keating <stephen.keating@gmail.com>
2020-07-21 14:50:39 -0400

    css for header and body

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Header = () => {
 
-  return <div className='header'>
-          This is Stephen's Portfolio
-        </div>
+  return <h1>
+          I'm a web developer and am currently seeking a new role. 
+          Here are a few projects that I've worked on recently. 
+        </h1>
 };
 
 export default Header;
