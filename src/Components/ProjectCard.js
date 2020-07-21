fec1c23deaa2a656f66d657e350dc00b9156bac4
Stephen Keating <stephen.keating@gmail.com>
2020-07-21 17:08:54 -0400

    making image links

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const ProjectCard = ({ even, project: {title, url, description, tech, bullets, github}}) => {
-console.log(even)
+const ProjectCard = ({ even, project: {title, url, description, tech, bullets, github, image}}) => {
+
   return <div className={'project-card' + (even ? ' even' : ' odd')}>
+          <img className='project-image' src={ require(`../Assets/${image}`) } />
           <div className='project-card-title'>
             <a href={url} target="_blank">{title}</a>
           </div>
           <div className='project-card-description'>
             { description }
           </div>
-          <div className='project-card-tech'>
+          {/* <div className='project-card-tech'>
             { tech.map((t, i) =>
                         <span>
                           { (i ? ' | ': '') + t }
@@ -22,7 +23,7 @@ console.log(even)
                             {'→ '}{ bullet }
                           </div>
               )}
-          </div>
+          </div> */}
           <div className='project-card-links'>
             <a href={url} target="_blank">Live</a> 
             {' | '}
