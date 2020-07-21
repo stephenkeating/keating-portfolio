ba79200c6b58961c9c247074f7e7502e85894908
Stephen Keating <stephen.keating@gmail.com>
2020-07-21 16:08:38 -0400

    formatting project cards

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -4,12 +4,25 @@ const ProjectCard = ({project: {title, url, description, tech, bullets}}) => {
 
   return <div className='project-card'>
           <div className='project-card-title'>
-            { title }
+            <a href={url}>{title}</a>
           </div>
-          { url }
-          { description }
-          { tech }
-          { bullets }
+          <div className='project-card-description'>
+            { description }
+          </div>
+          <div className='project-card-tech'>
+            { tech.map((t, i) =>
+                        <span>
+                          { (i ? ' | ': '') + t }
+                        </span>
+            )}
+          </div>
+          <div className='project-card-bullets'>
+            { bullets.map((bullet) =>
+                          <div>
+                            { bullet }
+                          </div>
+              )}
+          </div>
         </div>
 };
 
