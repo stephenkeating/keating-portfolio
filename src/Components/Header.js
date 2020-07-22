fc7a5cc8806a96487719721fd2d0de557e237061
Stephen Keating <stephen.keating@gmail.com>
2020-07-22 09:56:25 -0400

    cleaning up code

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Header = () => {
 
-  return <h1>
-          Web developer based in Brooklyn. Currently seeking a new role. 
-          Here are a few of my recent projects. 
-        </h1>
+  return (
+    <h1>
+      Web developer based in Brooklyn. Currently seeking a new role. 
+      Here are a few of my recent projects. 
+    </h1>
+  )
 };
 
 export default Header;
