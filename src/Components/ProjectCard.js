9613fbd7b60a2826eb0187c837c0d0a6284e4932
Stephen Keating <stephen.keating@gmail.com>
2020-07-22 08:50:47 -0400

    image text overlays in progress

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -3,12 +3,14 @@ import React from 'react';
 const ProjectCard = ({ even, project: {title, url, description, tech, bullets, github, image}}) => {
 
   return <div className={'project-card' + (even ? ' even' : ' odd')}>
+          <div className='project-image-container'>
             <img className='project-image' src={ require(`../Assets/${image}`) } />
             <div className='project-image-overlay'>
               <div className='project-image-text'>
-                '→ Project Details'
+                → Project Details
               </div>
             </div>
+          </div>
           <div className='project-card-title'>
             <a href={url} target="_blank">{title}</a>
           </div>
