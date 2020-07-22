d7f358228141fcb3395e8ed9e35a24345017e5f3
Stephen Keating <stephen.keating@gmail.com>
2020-07-22 15:37:23 -0400

    added history to browserrouter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
 import Nav from './Components/Nav';
+import { createBrowserHistory } from 'history';
 
 import './App.css';
 
 const App = () => {
+
+  const history = createBrowserHistory({
+    basename: 'base-name'
+  }) 
+
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL} history={history}>
       <Nav />
       <Routes />
     </Router>
