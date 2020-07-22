39f94a8ff6c540a4adaf02fad878f7555a3319c4
Stephen Keating <stephen.keating@gmail.com>
2020-07-22 09:55:23 -0400

    cleaning up code appearance

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -3,45 +3,47 @@ import { Link } from 'react-router-dom';
 
 const ProjectCard = ({ even, project: {title, url, description, tech, bullets, github, shortName}}) => {
 
-  return <div className={'project-card' + (even ? ' even' : ' odd')}>
-          <Link to={`/${shortName}`}>
-            <div className='project-image-container'>
-              <img className='project-image' src={ require(`../Assets/${shortName}.png`) } alt={`${title} screenshot`}/>
-              <div className='project-image-overlay'>
-                <div className='project-image-text'>
-                  → Project Details
-                </div>
-              </div>
+  return (
+    <div className={'project-card' + (even ? ' even' : ' odd')}>
+      <Link to={`/${shortName}`}>
+        <div className='project-image-container'>
+          <img className='project-image' src={ require(`../Assets/${shortName}.png`) } alt={`${title} screenshot`}/>
+          <div className='project-image-overlay'>
+            <div className='project-image-text'>
+              → Project Details
             </div>
-          </Link>
-          <div className='project-card-title'>
-            <Link to={`/${shortName}`}>{title}</Link>
-          </div>
-          <div className='project-card-description'>
-            { description }
-          </div>
-          {/* <div className='project-card-tech'>
-            { tech.map((t, i) =>
-                        <span>
-                          { (i ? ' | ': '') + t }
-                        </span>
-            )}
-          </div>
-          <div className='project-card-bullets'>
-            { bullets.map(bullet =>
-                          <div>
-                            {'→ '}{ bullet }
-                          </div>
-              )}
-          </div> */}
-          <div className='project-card-links'>
-            <a href={url} target="_blank" rel="noopener noreferrer">Live</a> 
-            {' | '}
-            <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
-            {' | '}
-            <Link to={`/${shortName}`}>Project Details</Link>
           </div>
         </div>
+      </Link>
+      <div className='project-card-title'>
+        <Link to={`/${shortName}`}>{title}</Link>
+      </div>
+      <div className='project-card-description'>
+        { description }
+      </div>
+      {/* <div className='project-card-tech'>
+        { tech.map((t, i) =>
+                    <span>
+                      { (i ? ' | ': '') + t }
+                    </span>
+        )}
+      </div>
+      <div className='project-card-bullets'>
+        { bullets.map(bullet =>
+                      <div>
+                        {'→ '}{ bullet }
+                      </div>
+          )}
+      </div> */}
+      <div className='project-card-links'>
+        <a href={url} target="_blank" rel="noopener noreferrer">Live</a> 
+        {' | '}
+        <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
+        {' | '}
+        <Link to={`/${shortName}`}>Project Details</Link>
+      </div>
+    </div>
+  )
 };
 
 export default ProjectCard;
