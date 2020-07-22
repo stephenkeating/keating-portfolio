d47bc60c3260cdf1692afc0ca7e9f9c076b50ced
Stephen Keating <stephen.keating@gmail.com>
2020-07-22 11:50:48 -0400

    cleaning up variables

--- src/Pages/Earworm.js
+++ src/Pages/Earworm.js
@@ -16,33 +16,35 @@ const Earworm = () => {
 
   return (
     <div className='project-page'>
-      <h1>
+      <h1 className='product-page-title'>
         {title}
       </h1>
       <h1>
         <img className='project-page-image' src={ require(`../Assets/${shortName}.png`) } alt={`${title} screenshot`}/>
       </h1>
-      <div className='project-page-title'>
+      <div className='project-page-sub-title'>
         {title}
       </div>
       <div className='project-page-description'>
         { description }
       </div>
-      <div className='project-card-tech'>
+      <div className='project-page-tech'>
+        {'Tech used: '}
         { tech.map((t, i) =>
                     <span>
                       { (i ? ' | ': '') + t }
                     </span>
         )}
       </div>
-      <div className='project-card-bullets'>
+      <div className='project-page-bullets'>
         { bullets.map(bullet =>
                       <div>
                         {'→ '}{ bullet }
                       </div>
           )}
       </div>
-      <div className='project-card-links'>
+      <br></br>
+      <div className='project-page-links'>
         <a href={url} target="_blank" rel="noopener noreferrer">Live</a> 
         {' | '}
         <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
