eba5db76c9812ec8d6a45a0aaf041f4dcf356b9a
Stephen Keating <stephen.keating@gmail.com>
2020-07-23 09:04:40 -0400

    testing project page layout

--- src/Pages/Earworm.js
+++ src/Pages/Earworm.js
@@ -19,36 +19,42 @@ const Earworm = () => {
       <h1 className='product-page-title'>
         {title}
       </h1>
+      {/* <div className='project-page-sub-title'>
+        {title}
+      </div> */}
+      <div className='project-page-row'>
+        <div className='project-page-column left-column'>
+          <div className='project-page-description'>
+            { description }
+          </div>
+          <div className='project-page-links'>
+            <a href={url} target="_blank" rel="noopener noreferrer">Live</a> 
+            {' | '}
+            <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
+          </div>
+          <div className='project-page-tech'>
+            {'Tech used: '}
+            <br></br>
+            { tech.map((t, i) =>
+                        <span>
+                          { (i ? ' | ': '') + t }
+                        </span>
+            )}
+          </div>
+        </div>
+        <div className='project-page-column'>
+          <div className='project-page-bullets'>
+            { bullets.map(bullet =>
+                          <div>
+                            {'→ '}{ bullet }
+                          </div>
+              )}
+          </div>
+        </div>
+      </div>
       <h1>
         <img className='project-page-image' src={ require(`../Assets/${shortName}.png`) } alt={`${title} screenshot`}/>
       </h1>
-      <div className='project-page-sub-title'>
-        {title}
-      </div>
-      <div className='project-page-description'>
-        { description }
-      </div>
-      <div className='project-page-tech'>
-        {'Tech used: '}
-        { tech.map((t, i) =>
-                    <span>
-                      { (i ? ' | ': '') + t }
-                    </span>
-        )}
-      </div>
-      <div className='project-page-bullets'>
-        { bullets.map(bullet =>
-                      <div>
-                        {'→ '}{ bullet }
-                      </div>
-          )}
-      </div>
-      <br></br>
-      <div className='project-page-links'>
-        <a href={url} target="_blank" rel="noopener noreferrer">Live</a> 
-        {' | '}
-        <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
-      </div>
     </div>
   )
 };
