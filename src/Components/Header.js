94da72f2c97df4e658954ae7b7c78edc8ec93477
Stephen Keating <stephen.keating@gmail.com>
2020-08-05 10:03:05 -0400

    changed bio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,7 @@ const Header = () => {
 
   return (
     <h1>
-      Web developer based in Brooklyn. Currently seeking a new role. 
-      Here are a few of my recent projects. 
+      My curiosity with tech started early–I was networking computers together in middle school so my friends and I could play video games. That turned into an internship at an internet startup during high school, during which I self-taught programming basics. Out of college, I chose a career in the humanities, achieved ABD for a PhD in philosophy of religion and founded a tutoring company. Now that I’m back in tech, I’m excited to bring my love of learning, collaboration, and scrappy work ethic to my coding career. I’m really proud of Earworm, a “name that tune” game that I built with React.js, Ruby on Rails, and Spotify APIs, and I also recently bought a Raspberry Pi to learn Linux and set up a video server for my 3-year-old (mostly Peppa Pig). I’d love to work at a company where diversity matters, where anyone can have a business impact, and where continuous learning is a core value. 
     </h1>
   )
 };
